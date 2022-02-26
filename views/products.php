5e39f11262b689dfd328d53f5225f89131c463dd
taulantn <tn47423@ubt-uni.net>
2022-02-26 16:34:10 +0100

    Products page improvements

--- views/products.php
+++ views/products.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../css/rooms.css">
+    <link rel="stylesheet" href="../css/products.css">
 </head>
 <body>
     <?php 
@@ -13,7 +13,35 @@
     ?>
     <div id="main">
         
-        <img src="../images/coming-soon5.png" alt="">
+        <ul id="nav-rooms">
+            <li><a href="">Living Room</a></li>
+            <li><a href="">Kitchen</a></li>
+            <li><a href="">Bedroom</a></li>
+            <li><a href="">Bathroom</a></li>
+            <li><a href="">Dining Room</a></li>
+            <li><a href="">Lightning</a></li>
+
+        </ul>
+
+        <div class="products-container">
+        <div class="products-panel wrapper">
+                <?php //foreach($products as $product){ 
+                    //$pid = $product['id'];
+                ?>
+                <input class="hidden" type="text" name="product_id" value=<?php $pid; ?>> 
+                    <div class="square">
+                        <div>
+                            <a href="<?php //echo "view-product.php?pid=$pid" ?>"><img src=<?php echo $product['image']; ?> alt=""></a>
+                        </div>
+                        <div>
+                            <h3><?php //echo $product['emri']; ?></h3>
+                            <h2><?php //echo $product['cmimi']; ?>&euro;</h2>
+                            <a class="button" href="<?php //echo "view-product.php?pid=$pid" ?>">Shiko Produktin</a>
+                        </div>
+                    </div>
+                <?php //} ?>
+            </div>
+        </div>
 
     </div>
     <?php 
--- views/rooms.php
+++ views/rooms.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Products | Homegoods</title>
-    <link rel="stylesheet" href="../css/products.css">
+    <link rel="stylesheet" href="../css/rooms.css">
     <script src="https://kit.fontawesome.com/92572388a0.js" crossorigin="anonymous"></script>
 
    
